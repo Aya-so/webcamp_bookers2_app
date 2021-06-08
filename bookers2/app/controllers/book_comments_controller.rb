a1dbc58c5d77e1a2c2d63f19dfdb3ef2c024500b
Aya-so <ayaka.wakame0315@gmail.com>
2021-06-08 16:49:42 +0000

    [Add]コメント機能の非同期通信

--- bookers2/app/controllers/book_comments_controller.rb
+++ bookers2/app/controllers/book_comments_controller.rb
@@ -20,6 +20,7 @@ class BookCommentsController < ApplicationController
     # BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     # redirect_to book_path(params[:book_id])
 
+    book = Book.find(params[:book_id])
     comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
     comment.destroy
     redirect_to book_path(params[:book_id])
